3c0bf54cd0838e07ece8d5b74be14e324226433f
Tola Tess <x17131553@student.ncirl.ie>
2020-05-20 18:25:24 +0100

    CurrentUser logic
    check if current user is logged in, if not navigate to start page or proceed to main activity

--- app/src/main/java/com/tolaotesanya/healthpad/MainActivity.java
+++ app/src/main/java/com/tolaotesanya/healthpad/MainActivity.java
@@ -9,31 +9,53 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.tolaotesanya.healthpad.profile.auth.AuthActivity;
+import com.tolaotesanya.healthpad.profile.auth.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MainActivity";
 
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     private Toolbar mToolbar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         attachDrawerNav();
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Ensure user is logged in before proceeding to main activity
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            sendToStart();
+        }
+    }
+
     public void attachDrawerNav() {
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.bringToFront();
 
         mToolbar = findViewById(R.id.main_page_toolbar);
@@ -68,15 +90,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             case R.id.nav_search:
                 break;
-            case R.id.nav_login:
-                break;
             case R.id.nav_account:
                 break;
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                sendToStart();
             case R.id.nav_chat:
                 break;
         }
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void sendToStart() {
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 
