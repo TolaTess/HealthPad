d1c4bfdfd7edbd71c629574f2d9f0506c785da56
Tola Tess <x17131553@student.ncirl.ie>
2020-05-20 15:20:26 +0100

    Register layout
    layout for registration added

---
+++ app/src/main/java/com/tolaotesanya/healthpad/profile/auth/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.tolaotesanya.healthpad.profile.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tolaotesanya.healthpad.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
