5a9238f6f47bea66442fb86796e1dbeaf49eaafe
Tola Tess <x17131553@student.ncirl.ie>
2020-05-29 10:46:11 +0100

    Chat Activity layout
    added chat activity to send messages to and from users

---
+++ app/src/main/java/com/tolaotesanya/healthpad/activities/chat/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.tolaotesanya.healthpad.activities.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tolaotesanya.healthpad.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
